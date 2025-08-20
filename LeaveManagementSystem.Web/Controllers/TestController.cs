6502520ba2a309a94b3d37c627e8b188b940baa7
Ána Vieira <acavieira02@gmail.com>
2025-08-20 16:28:39 +0100

    Add project files.

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using LeaveManagementSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Ana",
+                DateOfBirth = new DateTime(2002, 5, 6)
+            }; 
+            return View(data);
+        }
+    }
+}
